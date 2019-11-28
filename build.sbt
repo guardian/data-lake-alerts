0913b3cf3faeb11972e1dd3e01d32c46cf280f8c
Jacob Winch <jacob.winch@guardian.co.uk>
2019-11-28 15:17:39 +0000

    Add functionality for posting to SQS queue

--- build.sbt
+++ build.sbt
@@ -19,6 +19,7 @@ libraryDependencies ++= Seq(
   "com.amazonaws" % "aws-lambda-java-core" % "1.2.0",
   "com.amazonaws" % "aws-lambda-java-log4j2" % "1.1.0",
   "com.amazonaws" % "aws-java-sdk-ssm" % "1.11.604",
+  "com.amazonaws" % "aws-java-sdk-sqs" % "1.11.683",
   "com.gu" %% "anghammarad-client" % "1.0.4",
   "org.slf4j" % "slf4j-api" % "1.7.26",
   "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.8.2",
--- src/main/scala/com/gu/datalakealerts/Sqs.scala
+++ src/main/scala/com/gu/datalakealerts/Sqs.scala
@@ -1,5 +1,24 @@
 package com.gu.datalakealerts
 
+import com.amazonaws.services.sqs.AmazonSQSClient
+import com.amazonaws.services.sqs.model.{ SendMessageRequest, SendMessageResult }
+
 object Sqs {
+
+  def queueName(app: String, stage: String) = s"$app-$stage" //This is also defined in cfn.yml
+
+  private val sqsClient = AmazonSQSClient
+    .builder()
+    .withCredentials(AwsCredentials.ophanCredentials)
+    .build()
+
+  def enqueueRunningQuery(monitoringEventWithQueryInfo: MonitoringEventWithQueryInfo, queueName: String, delayInSeconds: Int): SendMessageResult = {
+    val queueUrl = sqsClient.getQueueUrl(queueName).getQueueUrl
+    val messageRequest = new SendMessageRequest()
+      .withQueueUrl(queueUrl)
+      .withMessageBody(monitoringEventWithQueryInfo.toJson)
+      .withDelaySeconds(delayInSeconds)
+    sqsClient.sendMessage(messageRequest)
+  }
 
 }
